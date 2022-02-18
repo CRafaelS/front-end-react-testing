4bc7d0af1f332f9fe0bb552d7a8b51824191897a
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-02-18 19:32:26 -0300

    Testando se tem a mensagem se nao tiver favorito e se favoritar verifica se tem o Cart, requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -71,7 +71,7 @@ test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', 
 
 test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
   const AboutHistory = createMemoryHistory();
-  const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+  const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
   render(
     <Router history={ AboutHistory }>
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+test('Teste se é redirecionada para a página de Pokémons Favoritados.', () => {
+  const favoritesHistory = createMemoryHistory();
+
+  render(
+    <Router history={ favoritesHistory }>
+      <App />
+    </Router>,
+  );
+
+  favoritesHistory.push('/favorites');
+  const noFavorite = screen.getByText(/no favorite pokemon found/i);
+  expect(noFavorite).toBeInTheDocument();
+});
+
+test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const clickHome = screen.getByRole('link', { name: 'Home' });
+  userEvent.click(clickHome);
+
+  const moreDetails = screen.getByRole('link', {
+    name: /more details/i,
+  });
+  userEvent.click(moreDetails);
+
+  const favoriteCheck = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  userEvent.click(favoriteCheck);
+
+  const clickFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+  userEvent.click(clickFavorite);
+
+  const pikachuFavorite = screen.getByRole('img', {
+    name: /pikachu is marked as favorite/i,
+  });
+  expect(pikachuFavorite).toBeInTheDocument();
+});
