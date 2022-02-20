d8ea6939e1e93d3ee67ad8132883eff7d39b214b
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-02-20 14:44:56 -0300

    Testes se o texto e a imagem aparece na pagina de erro 404, Requisito 4 satisfeito.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('Teste se página tem um heading h2 com o texto Page requested not found 😭', () => {
+  const notFoundHistory = createMemoryHistory();
+
+  render(
+    <Router history={ notFoundHistory }>
+      <App />
+    </Router>,
+  );
+
+  notFoundHistory.push('/notFound-page');
+
+  const notFoundSubTitle = screen.getByRole('heading', {
+    name: /Page requested not found /i,
+    level: 2,
+  });
+  expect(notFoundSubTitle).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+  const notFoundHistory = createMemoryHistory();
+  const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  render(
+    <Router history={ notFoundHistory }>
+      <App />
+    </Router>,
+  );
+
+  notFoundHistory.push('/notFound-page');
+  const findImag = screen.getByRole('img', {
+    name: /Pikachu crying because the page requested was not found/i,
+  });
+  expect(findImag.src).toBe(url);
+});
