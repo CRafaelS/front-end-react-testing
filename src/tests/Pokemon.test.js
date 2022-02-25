9bfa3bc62bfb1d6d7b02c502a889d5546546584c
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-02-25 19:16:44 -0300

    Testando o componente Pokemon. Requisito 6 satisfeito.

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import pokemons from '../data';
 
-test('Teste se a aplicação é redirecionada para a página inicial.', () => {
+test('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
   const homeHistory = createMemoryHistory();
   render(
     <Router history={ homeHistory }>
@@ -14,9 +16,83 @@ test('Teste se a aplicação é redirecionada para a página inicial.', () => {
 
   homeHistory.push('/');
 
-  const namePokemon = screen.getByText(/pikachu/i);
-  expect(namePokemon).toBeInTheDocument();
+  pokemons.forEach(({ name, type, averageWeight: { value, measurementUnit }, image }) => {
+    const namePokemon = screen.getByText(name);
+    const typePokemon = screen.getByTestId('pokemon-type');
+    const weightPokemon = screen.getByText(`Average weight: ${value} ${measurementUnit}`);
+    const imagePokémon = screen.getByRole('img', { name: `${name} sprite` });
+    expect(namePokemon).toBeInTheDocument();
+    expect(typePokemon.innerHTML).toBe(type);
+    expect(weightPokemon).toBeInTheDocument();
+    expect(imagePokémon).toBeInTheDocument();
+    expect(imagePokémon.src).toBe(image)
+    const nextButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+    userEvent.click(nextButton);
+  });
+});
 
-  const typePokemon = screen.getByText(/Electric/i);
-  expect(typePokemon).toBeInTheDocument();
+test('Teste se o card do Pokémon indicado na Pokédex contém um link detalhes.', () => {
+  const homeHistory = createMemoryHistory();
+  render(
+    <Router history={ homeHistory }>
+      <App />
+    </Router>,
+  );
+
+  homeHistory.push('/');
+
+  const { id } = pokemons[0];
+  const moreDetails = screen.getByRole('link', { name: /more details/i });
+  expect(moreDetails).toBeInTheDocument();
+  expect(moreDetails.href).toBe(`http://localhost/pokemons/${id}`);
+  userEvent.click(moreDetails);
+  expect(homeHistory.location.pathname).toBe(`/pokemons/${id}`);
+});
+
+test('Teste se ao clicar no link, é feito o redirecionamento da aplicação.', () => {
+  const homeHistory = createMemoryHistory();
+  render(
+    <Router history={ homeHistory }>
+      <App />
+    </Router>,
+  );
+
+  homeHistory.push('/');
+
+  const moreDetails = screen.getByRole('link', { name: /more details/i });
+  userEvent.click(moreDetails);
+  const sumary = screen.getByRole('heading', { name: /Summary/i, level: 2 });
+  const gameLocations = screen
+    .getByRole('heading', { name: /game Locations/i, level: 2 });
+  expect(sumary).toBeInTheDocument();
+  expect(gameLocations).toBeInTheDocument();
+});
+
+test('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+  const homeHistory = createMemoryHistory();
+  render(
+    <Router history={ homeHistory }>
+      <App />
+    </Router>,
+  );
+
+  homeHistory.push('/');
+
+  const iconStar = '/star-icon.svg';
+
+  const moreDetails = screen.getByRole('link', { name: /more details/i });
+  userEvent.click(moreDetails);
+
+  const favoriteCheck = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  userEvent.click(favoriteCheck);
+
+  const clickFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+  userEvent.click(clickFavorite);
+
+  const pikachuFavorite = screen.getByRole('img', {
+    name: /pikachu is marked as favorite/i,
+  });
+
+  expect(pikachuFavorite).toHaveAttribute('src', iconStar);
+  expect(pikachuFavorite).toHaveAttribute('alt', 'Pikachu is marked as favorite');
 });
